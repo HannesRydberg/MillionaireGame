e26afa2b410c21cb0d15d3495e4de8132e785585
Hannes Rydberg <mrhrxx@gmail.com>
2016-09-29 12:19:03 +0200

    First Commit. Just dipping my toe into the pond...

---
+++ Millionaire/Millionaire/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Millionaire
+{
+    partial class millionaireForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(235, 259);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(179, 23);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Test Button";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // millionaireForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 375);
+            this.Controls.Add(this.testButton);
+            this.Name = "millionaireForm";
+            this.Text = "Millionaire";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
